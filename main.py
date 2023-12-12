085465bc35bd23c18688f8e9555534016350d343
noé m <noe.moyen@epitech.eu>
2023-12-12 22:28:52 +0100

    merging all script to main, wip

--- main.py
+++ main.py
@@ -4,163 +4,142 @@ import re
 import json
 from outils.user_agents import read_user_agents, get_random_user_agent
 from tqdm import tqdm
+from bs4 import BeautifulSoup
+from usp.tree import sitemap_tree_for_homepage
 
+#---------------------------------------------------------------------- Variables
 requested_url = []
-to_request = []
+url_to_request = []
+all_urls = []
+global target_domain
 
-# Liste d'User-Agents lus à partir du fichier
 user_agents = read_user_agents('outils/data/user_agents.txt')
 headers = {'User-Agent': get_random_user_agent(user_agents)}
-
+#---------------------------------------------------------------------- Print in file
+def write_links_to_file(filename, links):
+    try:
+        with open(filename, 'a', encoding='utf-8') as file:
+            for link in links:
+                file.write(link + '\n')
+    except Exception as e:
+        print(f"Erreur lors de l'écriture dans le fichier {filename}: {e}")
+#---------------------------------------------------------------------- Utils
 def is_same_domain(url):
     # Vérifie si l'URL appartient au domaine cible
     parsed_url = urlparse(url)
     return parsed_url.netloc == target_domain
 
-def get_header_link_data(url):
-    requested_url.append(url)
-    print(url)
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        link_header = response.headers.get('Link')
-        return link_header
-    else: 
-        print("can't get Link data", response.status_code)
-
-
-def extract_url_in_link_data(data, base_url):
-    new_links = set()
+#---------------------------------------------------------------------- Extract link from xml
+def extract_link_from_xml():
+    http_target_url = "https://" + target_domain
+    tree = sitemap_tree_for_homepage(http_target_url)
+    for page in tree.all_pages():
+        if page.url not in requested_url:
+            url_to_request.append(page.url)
+#---------------------------------------------------------------------- Get subdomains
+def get_subdomains():
+    url = f'https://crt.sh/?q=%.{target_domain}&output=json'
     try:
-        base_url = base_url.rstrip('/')
-        matches = re.finditer(r'<([^>]+)>', data)
+        response = requests.get(url)
+        data = response.json()
+        subdomains = set()
+        for entry in data:
+            subdomain = entry['name_value'].strip()
+            subdomains.add(subdomain)
+        for link in subdomains:
+            links_to_process = url_to_request.copy()
+            print(links_to_process)
+            if link not in links_to_process and '*' not in link:
+                url_to_request.append(link)
+    except Exception as e:
+        print(f"Erreur lors de la récupération des sous-domaines : {e}")
+        return set()
+
+
+    
+#---------------------------------------------------------------------- extract urls from header Link
+def get_header_link_data(response):
+    link_header = response.headers.get('Link')
+    new_links = []
+    try:
+        base_url = target_domain.rstrip('/')
+        matches = re.finditer(r'<([^>]+)>', link_header)
         for match in matches:
             url = match.group(1)
             full_url = urljoin(base_url, url)
-            if is_same_domain(full_url):
-                new_links.add(full_url)
-        return new_links
+            if is_same_domain(full_url) and full_url not in url_to_request:
+                new_links.append(full_url)
+                url_to_request.append(full_url)
     except Exception as e:
-        print("Error:", e)
-        return new_links
+        print("No link in header Link.")
 
-def get_header_data_and_extract_url(url):
-    data = get_header_link_data(url)
-    print(data)
-    links = extract_url_in_link_data(data, url)
-    for link in links:
-        to_request.append(link)
-
-def write_links_to_file(filename, links):
-    with open(filename, 'a') as file:
-        for link in links:
-            file.write(link + '\n')
-
-def request_loop_to_found_url():
-    while len(to_request) > 0:
-        links_to_process = to_request.copy()
-        for link in links_to_process:
-            if link not in requested_url:
-                print("Récupération des données de", link)
-                new_links = request_to_found_new_url(link)
-                print("Requêtes restantes:", len(to_request), "Faites:", len(requested_url))
-                for new_link in new_links:
-                    if new_link not in requested_url and new_link not in to_request:
-                        to_request.append(new_link)
-                        write_links_to_file('to_request.txt', [new_link])
-                requested_url.append(link)
-                to_request.remove(link)
-                
-                # Analyser le contenu de la page à la recherche de la clé API
-                response = requests.get(link, headers=headers)
-                if response.status_code == 200:
-                    analyze_page_content(link, response.text)
-                    # Enregistrer le lien avec le code d'état dans le fichier 'requested.txt'
-                    write_links_to_file('requested.txt', [f"{link} - Status Code: {response.status_code}"])
-                else:
-                    # En cas d'échec de la requête, enregistrer le lien avec le code d'erreur
-                    write_links_to_file('requested.txt', [f"{link} - Status Code: {response.status_code}"])
-
-
-
-def search_api_key_in_page(content):
-    api_key_patterns = [
-    r'-----BEGIN RSA PRIVATE KEY-----',                                                                 # RSA private key
-    r'-----BEGIN DSA PRIVATE KEY-----',                                                                 # SSH (DSA) private key
-    r'-----BEGIN EC PRIVATE KEY-----',                                                                  # SSH (EC) private key
-    r'-----BEGIN PGP PRIVATE KEY BLOCK-----',                                                           # PGP private key block
-    r'AKIA[0-9A-Z]{16}',                                                                                # Amazon AWS Access Key ID
-    r'amzn\.mws\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',                         # Amazon MWS Auth Token
-    r'AKIA[0-9A-Z]{16}',                                                                                # AWS API Key
-    r'[g|G][i|I][t|T][h|H][u|U][b|B].*[\'|\"][0-9a-zA-Z]{35,40}[\'|\"]',                                # GitHub
-    r'AIza[0-9A-Za-z\\-_]{35}',                                                                         # Google API Key
-    r'[0-9]+-[0-9A-Za-z_]{32}\.apps\.googleusercontent\.com',                                           # Google YouTube OAuth
-    r'[h|H][e|E][r|R][o|O][k|K][u|U].*[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}',    # Heroku API Key
-    r'[a-zA-Z]{3,10}://[^/\\s:@]{3,20}:[^/\\s:@]{3,20}@.{1,100}["\'\\s]',                               # Password in URL
-    r'https://hooks.slack.com/services/T[a-zA-Z0-9_]{8}/B[a-zA-Z0-9_]{8}/[a-zA-Z0-9_]{24}',             # Slack Webhook
-    r'[t|T][w|W][i|I][t|T][t|T][e|E][r|R].*[1-9][0-9]+-[0-9a-zA-Z]{40}',                                # Twitter Access Token
-]
-
-    found_keys = []
-    total_patterns = len(api_key_patterns)
-
-    with tqdm(total=total_patterns, desc="Searching API Keys", unit="pattern") as pbar:
-        for pattern in api_key_patterns:
-            try:
-                matches = re.findall(pattern, content)
-                found_keys.extend(matches)
-                pbar.update(1)  # Met à jour la barre de progression pour chaque motif traité
-            except:
-                print("error regex")
-
-    return found_keys
-
-def analyze_page_content(url, content):
-    # Analyser le contenu de la page à la recherche de la clé API
-    api_key = search_api_key_in_page(content)
-
-    if api_key:
-        #print(f"Clé API trouvée sur la page {url}: {api_key}")
-        # Faites ce que vous devez faire avec la clé API, par exemple, l'enregistrer dans un fichier
-        with open('api_keys.txt', 'a') as api_file:
-            api_file.write(f"{api_key} {url}\n")
-
-def request_to_found_new_url(url):
-    response = requests.get(url, headers=headers)
+#---------------------------------------------------------------------- 
+def get_data_from_url(link):
+    response = requests.get("http://" + link, headers=headers)
+    get_header_link_data(response)
     if response.status_code == 200:
-        try:
+        try: 
             contenu_json = response.json()
             liens = []
-            
-            def is_valid_url(url):
-                parsed_url = urlparse(url)
-                return parsed_url.netloc == target_domain
-            
-            def explorer_json(obj):
-                if isinstance(obj, list):
-                    for item in obj:
-                        explorer_json(item)
-                elif isinstance(obj, dict):
-                    for key, value in obj.items():
-                        if key == 'href' or key == 'src' and isinstance(value, str) and is_same_domain(value) and is_valid_url(value):
-                            liens.append(value)
-                        elif isinstance(value, (list, dict)):
-                            explorer_json(value)
-            
-            explorer_json(contenu_json)
-            return liens
+            explorer_json(contenu_json, liens, "http://" + target_domain)
+            print("azeaze",url_to_request)
         except json.decoder.JSONDecodeError:
-            return []
-    else:
-        return []
-    
+            explorer_html(response.text)
 
+def explorer_html(response):
+    # Initialiser un ensemble pour stocker temporairement les URLs uniques
+    unique_urls = set()
+
+    # Créer un objet BeautifulSoup pour analyser le contenu HTML
+    soup = BeautifulSoup(response, 'html.parser')
+
+    # Trouver tous les éléments <a>, <img>, <link>, <script> avec l'attribut href ou src
+    resource_elements = soup.find_all(['a', 'img', 'link', 'script'], href=True) + soup.find_all(['a', 'img', 'link', 'script'], src=True)
+
+    # Extraire les URLs des attributs href ou src
+    for element in resource_elements:
+        if 'href' in element.attrs:
+            url = urljoin(target_domain, element['href'])
+            unique_urls.add(url)
+        elif 'src' in element.attrs:
+            url = urljoin(target_domain, element['src'])
+            unique_urls.add(url)
+
+    # Trouver les liens vers les fichiers PDF et TXT
+    file_links = soup.find_all('a', href=re.compile(r'\.pdf$|\.txt$', re.IGNORECASE))
+    for link in file_links:
+        file_url = urljoin(target_domain, link['href'])
+        unique_urls.add(file_url)
+    extracted_urls = list(unique_urls)
+    for url in extracted_urls:
+        if url not in url_to_request:
+            url_to_request.append(url)
+
+def explorer_json(obj, liens, target_domain):
+    if isinstance(obj, list):
+        for item in obj:
+            explorer_json(item, liens, target_domain)
+    elif isinstance(obj, dict):
+        for key, value in obj.items():
+            if key in ('href', 'src') and isinstance(value, str) and value not in url_to_request:
+                url_to_request.append(value)
+            elif isinstance(value, (list, dict)):
+                explorer_json(value, liens, target_domain)
+    
 def main():
     global target_domain
-    target_domain = "onda.ai"
-    url = "https://" + target_domain
-    
-    get_header_data_and_extract_url(url)
-    request_loop_to_found_url()
-    
+    target_domain = input("Veuillez entrer l'url a target : ")
+    get_data_from_url(target_domain)
+    for url in url_to_request:
+        print(url)
+    #extract_link_from_xml()
+    #get_subdomains()
+    write_links_to_file("found_xml.txt", url_to_request)
+    #while len(url_to_request) > 0:
+    #    links_to_process = url_to_request.copy()
+    #    for link in links_to_process:
+    #        if link not in requested_url:
+    #            get_data_from_url(link)
+
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,39 +1,26 @@
-from crtsh import crtshAPI
 import requests
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:115.0) Gecko/20100101 Firefox/115.0"
-}
-def find_subdomains_passive(domain):
-    subdomains = set()
-
-    # Interroger la base de données CRT.sh pour les sous-domaines
+def get_subdomains(domain):
+    url = f'https://crt.sh/?q=%.{domain}&output=json'
     try:
-        results = crtshAPI().search(f"%.{domain}")
-        for result in results:
-            subdomains.add(result['name_value'])
+        response = requests.get(url)
+        data = response.json()
+        subdomains = set()
+        for entry in data:
+            subdomain = entry['name_value'].strip()
+            subdomains.add(subdomain)
+        return subdomains
     except Exception as e:
-        print(f"Erreur lors de la recherche passive : {e}")
-
-    return list(subdomains)
+        print(f"Erreur lors de la récupération des sous-domaines : {e}")
+        return set()
 
 if __name__ == "__main__":
-    target_domain = "koodsisu.fi"
-    found_subdomains = find_subdomains_passive(target_domain)
+    target_domain = "snowpack.eu"
+    subdomains = get_subdomains(target_domain)
 
-for subdomain in found_subdomains:
-    # Ajouter le schéma "https://" au sous-domaine
-    full_url = f"https://{subdomain}"
-
-    try:
-        response = requests.get(full_url, headers=headers)
-        if response.status_code == 200:
-            print(full_url)
-        else:
-            continue
-    except requests.exceptions.RequestException as e:
-        #print(f"Erreur lors de la requête vers {full_url}: {e}")
-        continue
+    if subdomains:
+        print(f"Sous-domaines de {target_domain} :")
+        for subdomain in subdomains:
+            print(subdomain)
     else:
-        continue
-        #print(f"Aucun sous-domaine trouvé pour {target_domain}.")
+        print(f"Aucun sous-domaine trouvé pour {target_domain}.")
