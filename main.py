85bee9db848d8614037904ba7307809c513cba02
noé m <noe.moyen@epitech.eu>
2023-12-12 23:43:19 +0100

    continue merge

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 from usp.tree import sitemap_tree_for_homepage
 
 #---------------------------------------------------------------------- Variables
-requested_url = []
+url_requested = []
 url_to_request = []
 all_urls = []
 global target_domain
@@ -29,13 +29,24 @@ def is_same_domain(url):
     parsed_url = urlparse(url)
     return parsed_url.netloc == target_domain
 
+def is_subdomain(url):
+    """
+    Vérifie si l'URL donnée appartient à l'un des domaines dans la liste.
+    """
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc.lower()
+    if domain.endswith(target_domain):
+        return True
+    return False
+
 #---------------------------------------------------------------------- Extract link from xml
 def extract_link_from_xml():
     http_target_url = "https://" + target_domain
     tree = sitemap_tree_for_homepage(http_target_url)
     for page in tree.all_pages():
-        if page.url not in requested_url:
+        if page.url not in url_requested and page.url not in url_to_request:
             url_to_request.append(page.url)
+            all_urls.append(page.url)
 #---------------------------------------------------------------------- Get subdomains
 def get_subdomains():
     url = f'https://crt.sh/?q=%.{target_domain}&output=json'
@@ -47,10 +58,10 @@ def get_subdomains():
             subdomain = entry['name_value'].strip()
             subdomains.add(subdomain)
         for link in subdomains:
-            links_to_process = url_to_request.copy()
-            print(links_to_process)
-            if link not in links_to_process and '*' not in link:
+            u = url_to_request.copy()
+            if link not in u and '*' not in link:
                 url_to_request.append(link)
+                all_urls.append(link)
     except Exception as e:
         print(f"Erreur lors de la récupération des sous-domaines : {e}")
         return set()
@@ -67,36 +78,47 @@ def get_header_link_data(response):
         for match in matches:
             url = match.group(1)
             full_url = urljoin(base_url, url)
-            if is_same_domain(full_url) and full_url not in url_to_request:
+            if is_same_domain(full_url):
                 new_links.append(full_url)
-                url_to_request.append(full_url)
+            for link in new_links:
+                if link not in url_to_request and link not in url_requested:
+                    url_to_request.append(full_url)
+                    all_urls.append(full_url)
     except Exception as e:
-        print("No link in header Link.")
+        pass
 
 #---------------------------------------------------------------------- 
 def get_data_from_url(link):
-    response = requests.get("http://" + link, headers=headers)
-    get_header_link_data(response)
-    if response.status_code == 200:
-        try: 
-            contenu_json = response.json()
-            liens = []
-            explorer_json(contenu_json, liens, "http://" + target_domain)
-            print("azeaze",url_to_request)
-        except json.decoder.JSONDecodeError:
-            explorer_html(response.text)
+    url = "http://" + link
+    url_requested.append(link)
+    if is_subdomain(url):
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Lèvera une exception pour les erreurs HTTP (4xx, 5xx)
+            get_header_link_data(response)
+            print(url_to_request)
+            # Continuez votre logique de traitement des données...
+        except ConnectionError as ce:
+            pass
+        except requests.exceptions.RequestException as e:
+            pass
 
+
+            
+        #print(url_to_request)
+        #if response.status_code == 200:
+        #    try: 
+        #        contenu_json = response.json()
+        #        liens = []
+        #        explorer_json(contenu_json, liens, "http://" + target_domain)
+        #    except json.decoder.JSONDecodeError:
+        #        explorer_html(response.text)
+        #except ConnectionError as ce:
+        #    print(f"Erreur de connexion pour l'URL {url}")
 def explorer_html(response):
-    # Initialiser un ensemble pour stocker temporairement les URLs uniques
     unique_urls = set()
-
-    # Créer un objet BeautifulSoup pour analyser le contenu HTML
     soup = BeautifulSoup(response, 'html.parser')
-
-    # Trouver tous les éléments <a>, <img>, <link>, <script> avec l'attribut href ou src
     resource_elements = soup.find_all(['a', 'img', 'link', 'script'], href=True) + soup.find_all(['a', 'img', 'link', 'script'], src=True)
-
-    # Extraire les URLs des attributs href ou src
     for element in resource_elements:
         if 'href' in element.attrs:
             url = urljoin(target_domain, element['href'])
@@ -104,16 +126,15 @@ def explorer_html(response):
         elif 'src' in element.attrs:
             url = urljoin(target_domain, element['src'])
             unique_urls.add(url)
-
-    # Trouver les liens vers les fichiers PDF et TXT
     file_links = soup.find_all('a', href=re.compile(r'\.pdf$|\.txt$', re.IGNORECASE))
     for link in file_links:
         file_url = urljoin(target_domain, link['href'])
         unique_urls.add(file_url)
     extracted_urls = list(unique_urls)
     for url in extracted_urls:
-        if url not in url_to_request:
+        if url not in url_to_request and url not in url_requested:
             url_to_request.append(url)
+            all_urls.append(url)
 
 def explorer_json(obj, liens, target_domain):
     if isinstance(obj, list):
@@ -121,25 +142,30 @@ def explorer_json(obj, liens, target_domain):
             explorer_json(item, liens, target_domain)
     elif isinstance(obj, dict):
         for key, value in obj.items():
-            if key in ('href', 'src') and isinstance(value, str) and value not in url_to_request:
+            if key in ('href', 'src') and isinstance(value, str) and value not in url_to_request and value not in url_requested:
                 url_to_request.append(value)
+                all_urls.append(value)
             elif isinstance(value, (list, dict)):
                 explorer_json(value, liens, target_domain)
-    
+
+#----------------------------------------------------------------------
 def main():
     global target_domain
     target_domain = input("Veuillez entrer l'url a target : ")
-    get_data_from_url(target_domain)
-    for url in url_to_request:
-        print(url)
+    #get_data_from_url(target_domain)
+    url_to_request.append(target_domain)
     #extract_link_from_xml()
-    #get_subdomains()
-    write_links_to_file("found_xml.txt", url_to_request)
-    #while len(url_to_request) > 0:
-    #    links_to_process = url_to_request.copy()
-    #    for link in links_to_process:
-    #        if link not in requested_url:
-    #            get_data_from_url(link)
 
+    get_subdomains()
+    write_links_to_file("found_xml.txt", url_to_request)
+    while len(url_to_request) > 0:
+        links_to_process = url_to_request.copy()
+        for link in links_to_process:
+            if link not in url_requested:
+                url_to_request.remove(link)
+                get_data_from_url(link)
+                
+        print(len(url_to_request), len(url_requested))     
+    
 if __name__ == "__main__":
     main()
